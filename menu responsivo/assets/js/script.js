6e3b09504434628c1b01f65b7d8ccf6ace3225cb
Larissa Kich <76120366+Larissakich@users.noreply.github.com>
2022-03-01 23:17:17 -0300

    Add files via upload

---
+++ menu responsivo/assets/js/script.js
@@ -0,0 +1,10 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
